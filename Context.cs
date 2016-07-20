afac6bb6dbe0ff7b8608e20feacbce2b0f7692b9
Microeinstein <m1cr0th3bes7@gmail.com>
2016-07-20 15:14:53 +0200

    Aggiungere i file di progetto.

---
+++ Context.cs
@@ -0,0 +1,228 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Menu {
+    class Context : ApplicationContext {
+        public static ContextMenuStrip ListL, ListR;
+        public static Screen scr;
+        public static int closeFrom;
+        public static string userName;
+        string path, jsonFile;
+        NotifyIcon quickBarItem;
+        FormIcon fi;
+        ToolStripMenuItem _reload, _edit, _findIcon, _exit;
+        List<ToolStripItem> items;
+        List<Element> elements;
+        MethodInfo mi;
+
+        public Context() {
+            //var s = @"D:\\Documenti\\file inesistente.txt";
+            //s = Path.GetExtension(s);
+            quickBarItem = new NotifyIcon();
+            ListL = new ContextMenuStrip();
+            ListR = new ContextMenuStrip();
+            scr = Screen.FromControl(ListL);
+            closeFrom = 0;
+            userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            path = Path.Combine(Application.StartupPath, "menu.json");
+            jsonFile = Properties.Resources.defaultJson;
+            elements = new List<Element>();
+            mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            setLists();
+            setIcon();
+
+            _reload = new ToolStripMenuItem(
+                "Reload menu",
+                Properties.Resources.reload.ToBitmap(),
+                (a, b) => load());
+
+            _edit = new ToolStripMenuItem(
+                "Edit menu",
+                Properties.Resources.wrench.ToBitmap(),
+                edit);
+
+            _findIcon = new ToolStripMenuItem(
+                "Find icon",
+                Properties.Resources.binocular.ToBitmap(),
+                findIcon);
+
+            _exit = new ToolStripMenuItem(
+                "Exit",
+                Properties.Resources.cross.ToBitmap(),
+                close);
+
+            ListR.Items.AddRange(new ToolStripItem[] {
+                _reload,
+                _edit,
+                _findIcon,
+                new ToolStripSeparator(),
+                _exit });
+
+            quickBarItem.MouseUp += click;
+            ListL.Closing += listClosing;
+
+            load();
+        }
+        public void setLists() {
+            ListL.Name = "MenuL";
+            ListL.Size = new System.Drawing.Size(61, 4);
+            ListR.Name = "MenuR";
+            ListR.Size = new System.Drawing.Size(61, 4);
+        }
+        public void setIcon() {
+            quickBarItem.Icon = Properties.Resources.json;
+            quickBarItem.Text = "Menu";
+            quickBarItem.Visible = true;
+        }
+
+        public void load() {
+            quickBarItem.ShowBalloonTip(250, "", "Loading...", ToolTipIcon.None);
+
+            string balloon = "Loaded";
+            ListL.Items.Clear();
+
+            try {
+                items = new List<ToolStripItem>();
+                if (!File.Exists(path)) {
+                    File.Create(path).Close();
+                    File.WriteAllText(path, jsonFile);
+                    balloon = "Default menu loaded";
+                } else
+                    jsonFile = File.ReadAllText(path);
+                elements = JsonConvert.DeserializeObject<List<Element>>(jsonFile);
+
+                foreach (Element i in elements)
+                    items.Add(i.toMenuItem());
+                
+                ListL.Items.AddRange(items.ToArray());
+            } catch (Exception ex) {
+                balloon = "Error";
+                if (ex.ToString().Contains("Json"))
+                    MessageBox.Show("Error while parsing menu: \n\n" + ex.Message, "Error", 0, (MessageBoxIcon)48);
+                else
+                    MessageBox.Show("Program error: \n\n" + ex.ToString(), "Critical", 0, (MessageBoxIcon)16);
+            }
+            quickBarItem.ShowBalloonTip(250, "", balloon, ToolTipIcon.None);
+        }
+        public void click(object sender, MouseEventArgs e) {
+            quickBarItem.ContextMenuStrip = null;
+
+            if (e.Button == MouseButtons.Left && ListL.Items.Count > 0)
+                quickBarItem.ContextMenuStrip = ListL;
+            else if (e.Button == MouseButtons.Right)
+                quickBarItem.ContextMenuStrip = ListR;
+
+            if (quickBarItem.ContextMenuStrip != null)
+                mi.Invoke(quickBarItem, null);
+        }
+        public static void itemClick(object sender, MouseEventArgs e) {
+            ToolStripMenuItem s = (ToolStripMenuItem)sender;
+
+            string[] info = (string[])s.Tag;
+            if (e.Button != MouseButtons.Middle) {
+                closeFrom = 1;
+                try {
+                    if (info.Length >= 4) {
+                        ProcessStartInfo proc = new ProcessStartInfo(info[1], info[2]);
+                        if (info[3] != "")
+                            proc.WorkingDirectory = info[3];
+                        if (e.Button == MouseButtons.Right)
+                            proc.Verb = "runas";
+                        Process.Start(proc);
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show(
+                        "Failed to start " + Path.GetFileNameWithoutExtension(info[0]) +
+                        ":\n" + ex.Message +
+                        "\n\n" + "Path:" +
+                        "\n" + info[1],
+                        "Fail", 0, MessageBoxIcon.Information);
+                }
+            } else {
+                Process.Start("explorer.exe", @"/select," + info[1]);
+            }
+        }
+        public static void listClosing(object sender, ToolStripDropDownClosingEventArgs e) {
+            if (closeFrom == 2)
+                e.Cancel = true;
+            closeFrom = 0;
+        }
+
+        public void findIcon(object sender, EventArgs e) {
+            fi = new FormIcon();
+            fi.Show();
+        }
+        public void edit(object sender, EventArgs e) {
+            if (!File.Exists(path)) {
+                File.Create(path).Close();
+                File.WriteAllText(path, jsonFile);
+            }
+            Process.Start(path);
+        }
+        public void close(object sender, EventArgs e) {
+            quickBarItem.Visible = false;
+            Application.Exit();
+        }
+    }
+
+    public class ToolStripWatermarkTextbox : ToolStripTextBox {
+        public string Watermark { get; set; }
+        bool watermarked = true;
+        string _text = "";
+        public override string Text {
+            get { return _text; }
+            set {
+                _text = value;
+                base.Text = value;
+            }
+        }
+        public new event EventHandler TextChanged;
+
+        public ToolStripWatermarkTextbox(string watermark) : base() {
+            Watermark = watermark;
+            base.TextChanged += txtChg;
+            toWatermark();
+            GotFocus += (a, b) => toNormal();
+            LostFocus += (a, b) => toWatermark();
+        }
+
+        void txtChg(object sender, EventArgs e) {
+            if (!toNormal())
+                toWatermark();
+            if (!watermarked && TextChanged != null)
+                TextChanged(sender, e);
+        }
+        bool toNormal() {
+            if (_text != "" || Focused) {
+                Console.WriteLine(watermarked);
+                if (watermarked) {
+                    base.Text = "";
+                    base.ForeColor = System.Drawing.Color.Black;
+                    base.Font = new System.Drawing.Font(base.Font, System.Drawing.FontStyle.Regular);
+                    watermarked = false;
+                }
+                return true;
+            }
+            return false;
+        }
+        bool toWatermark() {
+            if (_text == "" && !Focused) {
+                if (!watermarked) {
+                    watermarked = true;
+                    base.Text = Watermark;
+                    base.ForeColor = System.Drawing.Color.Gray;
+                    base.Font = new System.Drawing.Font(base.Font, System.Drawing.FontStyle.Italic);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ FormIcon.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Menu {
+    partial class FormIcon {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormIcon));
+            this.img16 = new System.Windows.Forms.PictureBox();
+            this.numIcon = new System.Windows.Forms.NumericUpDown();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.img32 = new System.Windows.Forms.PictureBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.img16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img32)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img16
+            // 
+            this.img16.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.img16.BackColor = System.Drawing.Color.Transparent;
+            this.img16.Location = new System.Drawing.Point(384, 35);
+            this.img16.Name = "img16";
+            this.img16.Size = new System.Drawing.Size(16, 16);
+            this.img16.TabIndex = 0;
+            this.img16.TabStop = false;
+            // 
+            // numIcon
+            // 
+            this.numIcon.Location = new System.Drawing.Point(87, 42);
+            this.numIcon.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numIcon.Name = "numIcon";
+            this.numIcon.Size = new System.Drawing.Size(84, 20);
+            this.numIcon.TabIndex = 1;
+            this.numIcon.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numIcon.ValueChanged += new System.EventHandler(this.refresh);
+            // 
+            // txtPath
+            // 
+            this.txtPath.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtPath.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtPath.Location = new System.Drawing.Point(50, 16);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(317, 20);
+            this.txtPath.TabIndex = 2;
+            this.txtPath.Text = "C:\\Windows\\System32\\imageres.dll";
+            this.txtPath.TextChanged += new System.EventHandler(this.refresh);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Icon number:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(373, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.refresh);
+            // 
+            // img32
+            // 
+            this.img32.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.img32.BackColor = System.Drawing.Color.Transparent;
+            this.img32.Location = new System.Drawing.Point(406, 19);
+            this.img32.Name = "img32";
+            this.img32.Size = new System.Drawing.Size(32, 32);
+            this.img32.TabIndex = 0;
+            this.img32.TabStop = false;
+            // 
+            // txtResult
+            // 
+            this.txtResult.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtResult.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtResult.Location = new System.Drawing.Point(58, 68);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(309, 20);
+            this.txtResult.TabIndex = 2;
+            this.txtResult.Text = "C:\\Windows\\System32\\imageres.dll,0";
+            this.txtResult.TextChanged += new System.EventHandler(this.refresh);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Result:";
+            // 
+            // FormIcon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 102);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.numIcon);
+            this.Controls.Add(this.img32);
+            this.Controls.Add(this.img16);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormIcon";
+            this.Text = "Find icon";
+            this.Load += new System.EventHandler(this.FormIcon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.img16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img32)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img16;
+        private System.Windows.Forms.NumericUpDown numIcon;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox img32;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+    }
+}
